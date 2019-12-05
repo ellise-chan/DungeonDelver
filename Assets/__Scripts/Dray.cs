c81c73ba813f296c3aa2a656f0ad3863021e89ff
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-12-04 23:13:03 -0500

    Created GridMove Script and Updates Skeletos Script

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dray : MonoBehaviour
+public class Dray : MonoBehaviour, IFacingMover
 {
     public enum eMode { idle, move, attack, transition }
 
@@ -20,6 +20,7 @@ public class Dray : MonoBehaviour
 
     private Rigidbody rigid;
     private Animator anim;
+    private InRoom inRm;
 
     private Vector3[] directions = new Vector3[] {Vector3.right, Vector3.up, Vector3.left, Vector3.down };
     private KeyCode[] keys = new KeyCode[] { KeyCode.RightArrow,KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow };
@@ -28,6 +29,7 @@ public class Dray : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        inRm = GetComponent<InRoom>();
     }
 
     void Update()
@@ -84,5 +86,40 @@ public class Dray : MonoBehaviour
                 break;
         }
         rigid.velocity = vel * speed;
+    }
+
+    // Implementation of IFacingMover
+    public int GetFacing()
+    { 
+        return facing;
+    }
+    public bool moving
+    { 
+        get
+        {
+            return (mode == eMode.move);
+        }
+    }
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    public float gridMult
+    {
+        get { return inRm.gridMult; }
+    }
+    public Vector2 roomPos
+    { // f
+        get { return inRm.roomPos; }
+        set { inRm.roomPos = value; }
+    }
+    public Vector2 roomNum
+    {
+        get { return inRm.roomNum; }
+        set { inRm.roomNum = value; }
+    }
+    public Vector2 GetRoomPosOnGrid(float mult = -1)
+    {
+        return inRm.GetRoomPosOnGrid(mult);
     }
 }
