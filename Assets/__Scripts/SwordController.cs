3979e29b8b5b018de3f1f09920adab5ec6cae99f
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-12-04 22:37:41 -0500

    Update SwordController Script

--- Assets/__Scripts/SwordController.cs
+++ Assets/__Scripts/SwordController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SwordController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject sword;
+    private Dray dray;
+
     void Start()
     {
+        sword = transform.Find("Sword").gameObject; 
+        dray = transform.parent.GetComponent<Dray>();
         
+        // Deactivate the sword
+        sword.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.Euler(0, 0, 90 * dray.facing);
+        sword.SetActive(dray.mode == Dray.eMode.attack);
     }
 }
