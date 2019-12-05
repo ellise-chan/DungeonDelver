6cdfa5715543f5f13941b956d0cc80670e83c471
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-12-05 17:34:47 -0500

    Save

--- Assets/__Scripts/TileCamera.cs
+++ Assets/__Scripts/TileCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [System.Serializable]
 public class TileSwap
-{ 
+{
     public int tileNum;
     public GameObject swapPrefab;
     public GameObject guaranteedItemDrop;
@@ -19,42 +19,42 @@ public class TileCamera : MonoBehaviour
     static public Transform TILE_ANCHOR;
     static public Tile[,] TILES;
     static public string COLLISIONS;
-
     [Header("Set in Inspector")]
     public TextAsset mapData;
     public Texture2D mapTiles;
     public TextAsset mapCollisions; // This will be used later
     public Tile tilePrefab;
-    public int defaultTileNum; 
+    public int defaultTileNum;
     public List<TileSwap> tileSwaps;
-
-    private Dictionary<int, TileSwap> tileSwapDict; 
+    private Dictionary<int, TileSwap> tileSwapDict;
     private Transform enemyAnchor, itemAnchor;
 
     void Awake()
     {
         COLLISIONS = Utils.RemoveLineEndings(mapCollisions.text);
         PrepareTileSwapDict();
-        enemyAnchor = (new GameObject("Enemy Anchor")).transform;
-        itemAnchor = (new GameObject("Item Anchor")).transform;
+        enemyAnchor = (new GameObject("Enemy Anchor")).transform;
+        itemAnchor = (new GameObject("Item Anchor")).transform;
         LoadMap();
-    }    public void LoadMap()
+    }
+
+    public void LoadMap()
     {
         // Create the TILE_ANCHOR. All Tiles will have this as their parent.
         GameObject go = new GameObject("TILE_ANCHOR");
         TILE_ANCHOR = go.transform;
-        
+       
         // Load all of the Sprites from mapTiles
-        SPRITES = Resources.LoadAll<Sprite>(mapTiles.name); 
-        
+        SPRITES = Resources.LoadAll<Sprite>(mapTiles.name);
+        
         // Read in the map data
-        string[] lines = mapData.text.Split('\n'); 
+        string[] lines = mapData.text.Split('\n');
         H = lines.Length;
-        string[] tileNums = lines[0].Split(' ');
+        string[] tileNums = lines[0].Split(' '); // Keep an eye on this '' vs ' '
         W = tileNums.Length;
-        System.Globalization.NumberStyles hexNum; 
+
+        System.Globalization.NumberStyles hexNum;
         hexNum = System.Globalization.NumberStyles.HexNumber;
-        
         // Place the map data into a 2D Array for faster access
         MAP = new int[W, H];
         for (int j = 0; j < H; j++)
@@ -73,16 +73,13 @@ public class TileCamera : MonoBehaviour
                 CheckTileSwaps(i, j);
             }
         }
-        print("Parsed " + SPRITES.Length + "sprites."); 
+        print("Parsed " + SPRITES.Length + "sprites.");
         print("Map size: " + W + "wide by " + H + "high");
 
         ShowMap();
     }
 
-    /// <summary>
-    /// Generates Tiles for the entire map all at once.
-    /// </summary>
-
+    // Generates Tiles for the entire map all at once.
     void ShowMap()
     {
         TILES = new Tile[W, H];
@@ -95,29 +92,30 @@ public class TileCamera : MonoBehaviour
                 {
                     Tile ti = Instantiate<Tile>(tilePrefab);
                     ti.transform.SetParent(TILE_ANCHOR);
-                    ti.SetTile(i, j); 
+                    ti.SetTile(i, j);
                     TILES[i, j] = ti;
                 }
             }
         }
     }
 
-        void PrepareTileSwapDict()
-    { 
+    void PrepareTileSwapDict()
+    {
         tileSwapDict = new Dictionary<int, TileSwap>();
         foreach (TileSwap ts in tileSwaps)
         {
             tileSwapDict.Add(ts.tileNum, ts);
         }
     }
+
     void CheckTileSwaps(int i, int j)
-    { 
+    {
         int tNum = GET_MAP(i, j);
         if (!tileSwapDict.ContainsKey(tNum)) return;
-        // We do need to swap a tile
+        // We do need to swap a tile
         TileSwap ts = tileSwapDict[tNum];
         if (ts.swapPrefab != null)
-        { 
+        {
             GameObject go = Instantiate(ts.swapPrefab);
             Enemy e = go.GetComponent<Enemy>();
             if (e != null)
@@ -128,18 +126,21 @@ public class TileCamera : MonoBehaviour
             {
                 go.transform.SetParent(itemAnchor);
             }
-            go.transform.position = new Vector3(i, j, 0);
+            go.transform.position =
+            new Vector3(i, j, 0);
+
             if (ts.guaranteedItemDrop != null)
-            { 
+            {
                 if (e != null)
                 {
                     e.guaranteedItemDrop = ts.guaranteedItemDrop;
                 }
             }
         }
-        // Replace with another tile
+
+        // Replace with another tile
         if (ts.overrideTileNum == -1)
-        { // h
+        {
             SET_MAP(i, j, defaultTileNum);
         }
         else
@@ -149,7 +150,7 @@ public class TileCamera : MonoBehaviour
     }
 
     static public int GET_MAP(int x, int y)
-    { 
+    {
         if (x < 0 || x >= W || y < 0 || y >= H)
         {
             return -1; // Do not allow IndexOutOfRangeExceptions
@@ -158,19 +159,21 @@ public class TileCamera : MonoBehaviour
     }
 
     static public int GET_MAP(float x, float y)
-    { // A float GET_MAP() overload
+    { // A float GET_MAP() overload;
         int tX = Mathf.RoundToInt(x);
-        int tY = Mathf.RoundToInt(y - 0.25f); 
+        int tY = Mathf.RoundToInt(y - 0.25f);
         return GET_MAP(tX, tY);
     }
 
     static public void SET_MAP(int x, int y, int tNum)
-    { 
-      // Additional security or a break point could be set here.
+    {
+        // Additional security or a break point could be set here.
         if (x < 0 || x >= W || y < 0 || y >= H)
         {
             return; // Do not allow IndexOutOfRangeExceptions
         }
         MAP[x, y] = tNum;
     }
+
+
 }
