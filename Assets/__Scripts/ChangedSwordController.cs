25eb28b21b04dd922874a17d6ce390a3e6cfdf86
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-12-11 00:18:42 -0500

    Added new Features

---
+++ Assets/__Scripts/ChangedSwordController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangedSwordController : MonoBehaviour
+{
+    private GameObject sword;
+    private ChangedDray dray;
+
+    void Start()
+    {
+        sword = transform.Find("Sword").gameObject;
+        dray = transform.parent.GetComponent<ChangedDray>();
+
+        // Deactivate the sword
+        sword.SetActive(false);
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 90 * dray.facing);
+        sword.SetActive(dray.mode == ChangedDray.eMode.attack);
+    }
+}
